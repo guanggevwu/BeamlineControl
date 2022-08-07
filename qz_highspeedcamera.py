7e45ef329feff0c476cc4c645e60316e73404a10
Qing <guanggevwu@gmail.com>
2022-08-07 17:47:08 -0500

    after july exp

--- qz_highspeedcamera.py
+++ qz_highspeedcamera.py
@@ -1,4 +1,5 @@
 import os
+from this import d
 import time
 import logging
 # from highspeedcamera import PhotronDelay
@@ -29,7 +30,7 @@ IDDO="ID07us:Start.VAL"
 TrigNum = "7idbSAZ:randomScan:num"
 randomScan="7idbSAZ:randomScan:do"
 CamMode=""
-
+IDC_DG2_trigger = "7idc1:DG2:TriggerDelayBO"
 
 class beam():
     def __init__(self,timing) -> None:
@@ -103,7 +104,7 @@ class PhotronCam():
 
 
 beam1=beam([0.0,1.0,2.0])
-Photron=PhotronCam('E:\\CameraData4T\\2022.03\\Kefico')
+Photron=PhotronCam('E:\\CameraData4T\\2022.07\\Astemo')
 
 def close_shutter():
     caput(shutterA_close, 1, wait=True)
@@ -121,8 +122,7 @@ def open_shutter():
 
 
 
-def random_scan(spmain=None, triggermode='external', tile='T0', eachr=10, repeats=2, delay=1, 
-    ST=0, timelength=12):
+def random_scan(spmain=None, triggermode='external', tile='T0', eachr=10, repeats=2, delay=1, ST=0, timelength=12, sendTTL=1):
     """
     spmain format: "abc\\edf", not "\\abc\\edf" or "abc\\edf\\"
     """
@@ -133,7 +133,9 @@ def random_scan(spmain=None, triggermode='external', tile='T0', eachr=10, repeat
     caput(randomFramepv,eachr)
     # caput("7idbSAZ:HDF1:EnableCallbacks",0)
     # open_shutter()
-    if ST: open_shutter()
+    if ST: 
+        open_shutter()
+        time.sleep(2)
     if triggermode == 'manual':
         i=0
         while i < repeats:
@@ -148,33 +150,63 @@ def random_scan(spmain=None, triggermode='external', tile='T0', eachr=10, repeat
     # The external trigger can either be from 7idb:DG1 "internal" with a fixed rate, or an external signal sent to 7idb:DG1 with "Ext rising edge"
     elif triggermode == 'external':
         # caput('7idb:DG1:TriggerSourceMO',1)
+        # sendTTL is to send signal to external device, telling that the stages have been arrived the set position and the camera is ready to receive trigger
+        if sendTTL:
+            caput(Trig, 1)
         while caget('7idbSAZ:cam1:StatusName_RBV')!=4:
             time.sleep(0.1)
-        logging.info('receiving triggering...')
+        logging.info('receiving trigger...')
         time.sleep(timelength)
         logging.info(f'{timelength} s window of receiving trigger finished')                
         caput(TrigDone,1)
         # input('press enter to continue...')  
-        # caput('7idb:DG1:TriggerSourceMO',5)          
+        # caput('7idb:DG1:TriggerSourceMO',5)
     if ST: close_shutter()
 
-xlist1=[0,3.3,7.34,10.64]
-ylist1=[0,0.05,0.05,0]
-def position_scan(spmain=None, xlist=xlist1, ylist=ylist1, triggermode='external', eachr=10,
-    repeats=2, delay=1, ST=0, timelength=12):
+xlist1=[0, 3.4, 7.3, 10.7, 3.4, 3.4, 7.3, 7.3]
+ylist1=[0, 0.05, 0.05, 0, -5.6, -6.6, -5.6, -6.6]
+conditionlist_num = np.arange(0,0)
+# for condition 17 to 26, use conditionlist_num = np.arange(1,11) + 16
+# 13 to 18, use conditionlist_num = np.arange(1,7) + 12
+# 19 to 24, use conditionlist_num = np.arange(1,7) + 18
+# for condition 25 to 31, use conditionlist_num = np.arange(1,6) + 24
+# conditionlist = [str(i) for i in conditionlist_num]
+conditionlist = [""]
+
+# xlist1=[0, 3.5, 6.9, 10.4, 3.6, 6.8]
+# ylist1=[0, 0, 0, 0, -1.28, -1.28]
+def position_scan(spmain=None, xlist=xlist1, ylist=ylist1, triggermode='external', eachr=10, repeats=2, delay=1, ST=0, timelength=12, sendTTL=1):
+    caput("7idbSAZ:cam1:AcquireMode", 0)
+    time.sleep(0.5)
+    caput("7idbSAZ:cam1:AcquireMode", 1)
+    time.sleep(0.5)
+    i=0
     if not xlist or not ylist:
         logging.info('no scan positions provided')
         return
-    i=0
-    for x,y  in zip(xlist,ylist):
-        i+=1
-        caput(chamberX,x,wait=True)
-        caput(chamberY,y,wait=True)
-        if i != 1:
-            time.sleep(delay)
-            logging.info(f'delay finished')
-        logging.info(f'now at {i}th location: X = {x} mm and Y = {y} mm')
-        random_scan(spmain=spmain,triggermode=triggermode,tile=f'T{i}',eachr=eachr,repeats=repeats,
-        delay=1, ST=ST, timelength=timelength)
-    caput(chamberX,xlist[0],wait=True)
-    caput(chamberY,ylist[0],wait=True)    
+    for cond in conditionlist:
+
+        for x,y  in zip(xlist,ylist):
+            # print(cond)
+            if cond != "":
+                spmain_with_cond = os.path.join(spmain, cond)
+                hint1 = f'now under {cond} condition, and at {i}th location: X = {x} mm and Y = {y} mm'
+            else:
+                spmain_with_cond = spmain
+                hint1 = f'now at {i}th location: X = {x} mm and Y = {y} mm'
+            i+=1
+            if i != 1:
+                # time.sleep(delay)
+                while caget('7idbSAZ:TIFF1:FileNumber_RBV') < eachr * repeats + 1:
+                    time.sleep(0.1)
+                logging.info(f'{eachr * repeats} images are finished')
+                time.sleep(delay)
+                logging.info(f'delay finished')
+            caput(chamberX,x,wait=True)
+            caput(chamberY,y,wait=True)
+
+            logging.info(hint1)
+            random_scan(spmain=spmain_with_cond,triggermode=triggermode,tile=f'T{i}',eachr=eachr,repeats=repeats,
+            delay=1, ST=ST, timelength=timelength, sendTTL=sendTTL)
+        caput(chamberX,xlist[0],wait=True)
+        caput(chamberY,ylist[0],wait=True)         
